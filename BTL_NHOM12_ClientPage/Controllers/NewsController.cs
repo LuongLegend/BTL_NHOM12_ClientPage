806d366f2fa926bba14e4b8f664de21780c8030c
LuongLegend <luongprolv99@gmail.com>
2020-12-31 16:32:43 +0700

    add LinQ

---
+++ BTL_NHOM12_ClientPage/Controllers/NewsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BTL_NHOM12_ClientPage.Models;
+namespace BTL_NHOM12_ClientPage.Controllers
+{
+    public class NewsController : Controller
+    {
+        LinqDoNgoaiChinhHangDataContext db = new LinqDoNgoaiChinhHangDataContext();
+        // GET: News
+        public ActionResult Index()
+        {
+            var z = from a in db.News select a;
+            var zl = from a in db.News where a.new_ID == Request["idgiday"] select a;
+            ViewBag.news = z;
+            ViewData["news"] = z;
+            return View(zl);
+        }
+    }
+}

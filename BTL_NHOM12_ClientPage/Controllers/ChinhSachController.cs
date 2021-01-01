f3d1128d5df7c01d73e8730bfa90467fde492cb6
LuongLegend <luongprolv99@gmail.com>
2021-01-01 14:31:00 +0700

    done basic layout

---
+++ BTL_NHOM12_ClientPage/Controllers/ChinhSachController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_NHOM12_ClientPage.Controllers
+{
+    public class ChinhSachController : Controller
+    {
+        // GET: ChinhSach
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}

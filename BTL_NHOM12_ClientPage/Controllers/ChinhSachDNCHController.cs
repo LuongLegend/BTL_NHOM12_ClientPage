4b2619bfbc2df46ea5581784900f9726b951cfb5
NamNguyen2303 <nguyenvannam230399@gmail.com>
2021-01-03 23:07:29 +0700

    update huong dan va tin tuc

--- BTL_NHOM12_ClientPage/Controllers/ChinhSachDNCHController.cs
+++ BTL_NHOM12_ClientPage/Controllers/ChinhSachDNCHController.cs
@@ -4,21 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BTL_NHOM12_ClientPage.Models;
+
 namespace BTL_NHOM12_ClientPage.Controllers
 {
-    public class NewsController : Controller
+    public class ChinhSachDNCHController : Controller
     {
         LinqDoNgoaiChinhHangDataContext db = new LinqDoNgoaiChinhHangDataContext();
-        // GET: News
+        // GET: ChinhSachDNCH
         public ActionResult Index()
         {
-            var z = from a in db.News select a;
-            var zl = from a in db.News where a.new_ID == Request["idgiday"] select a;
             var cate = from a in db.Categories select a;
-            ViewBag.cates = cate;
-            ViewBag.news = z;
-            ViewData["news"] = z;
-            return View(zl);
+            return View(cate);
         }
     }
 }

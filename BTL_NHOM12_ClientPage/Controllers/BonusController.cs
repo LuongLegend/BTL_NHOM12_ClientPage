70e4ba25da9dc08fcfa422cc4cf1a71ee4bc3b45
vanhung1999dev <vanhung1999dev@gmail.com>
2021-01-04 23:11:26 +0700

    implement controller_bonus and view

--- BTL_NHOM12_ClientPage/Controllers/BonusController.cs
+++ BTL_NHOM12_ClientPage/Controllers/BonusController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc;
+using BTL_NHOM12_ClientPage.Models;
 
 namespace BTL_NHOM12_ClientPage.Controllers
 {
     public class BonusController : Controller
     {
+        LinqDoNgoaiChinhHangDataContext db = new LinqDoNgoaiChinhHangDataContext();
+
         // GET: Bonus
         public ActionResult Index()
         {
+            var list_bonus = from bonus in db.Bonus select bonus;
+            ViewBag.list_bonus = list_bonus;
+
             return View();
         }
     }
--- BTL_NHOM12_ClientPage/Models/Bonus.cs
+++ BTL_NHOM12_ClientPage/Models/Bonus.cs
@@ -10,7 +10,6 @@ namespace BTL_NHOM12_ClientPage.Models
         public string bonus_ID { get; set; }
         public string name_bonus { get; set; }
         public string photo { get; set; }
-
         public int price { get; set; }
     }
 }

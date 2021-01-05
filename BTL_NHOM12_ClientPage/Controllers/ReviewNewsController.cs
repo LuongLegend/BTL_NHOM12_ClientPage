96e2cb1982aaff4a358ae1590cb330e8eddf0c0e
NamNguyen2303 <nguyenvannam230399@gmail.com>
2021-01-05 22:28:24 +0700

    update Review News

---
+++ BTL_NHOM12_ClientPage/Controllers/ReviewNewsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BTL_NHOM12_ClientPage.Models;
+namespace BTL_NHOM12_ClientPage.Controllers
+{
+    public class ReviewNewsController : Controller
+    {
+        LinqDoNgoaiChinhHangDataContext db = new LinqDoNgoaiChinhHangDataContext();
+        // GET: ReviewNews
+        public ActionResult Index(string id)
+        {
+            var title = from a in db.News
+                    where a.new_ID == id
+                    select a;
+            var cate = from a in db.Categories select a;
+            ViewBag.cates = cate;
+            //ViewBag.tt = title;
+            return View(title);
+        }
+    }
+}

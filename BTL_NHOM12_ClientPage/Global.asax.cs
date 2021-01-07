f8771ad2eb7398f7bbf21fa21a7d2dbbbc7163fe
LuongLegend <luongprolv99@gmail.com>
2021-01-07 18:01:58 +0700

    done Part 2. Add current User. Total User

--- BTL_NHOM12_ClientPage/Global.asax.cs
+++ BTL_NHOM12_ClientPage/Global.asax.cs
@@ -16,6 +16,22 @@ namespace BTL_NHOM12_ClientPage
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.IO.StreamReader sr;
+            sr = new System.IO.StreamReader(Server.MapPath("~/totalVisitedUser.txt"));
+            string numUser = sr.ReadLine();
+            sr.Close();
+            Application["totalVisitedUser"] = numUser;
+            Application["currentUser"] = "0";
+        }
+
+        protected void Session_Start()
+        {
+            HttpContext.Current.Application["currentUser"] = int.Parse(HttpContext.Current.Application["currentUser"].ToString()) + 1;
+            HttpContext.Current.Application["totalVisitedUser"] = int.Parse(HttpContext.Current.Application["totalVisitedUser"].ToString()) + 1;
+            System.IO.StreamWriter sw;
+            sw = new System.IO.StreamWriter(Server.MapPath("totalVisitedUser.txt"));
+            sw.Write(HttpContext.Current.Application["totalVisitedUser"].ToString());
+            sw.Close();
         }
     }
 }
